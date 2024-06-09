15204767250deabb0303b50ce84a4e4be536ced1
Owen Zhang <owen.z.0523@gmail.com>
2024-06-09 15:31:55 -0400

    finished code

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -11,7 +11,7 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
-import { useSearchParams } from 'next/navigation'
+import { useRouter, useSearchParams } from 'next/navigation'
 import EndCallButton from './EndCallButton'
 import Loader from './Loader'
 
@@ -23,6 +23,7 @@ const MeetingRoom = () => {
   const isPersonalRoom = !!searchParams.get('personal')
   const [layout, setLayout] = useState<CallLayoutType>('speaker-left')
   const [showParticipants, setShowParticipants] = useState(false)
+  const router = useRouter();
 
   const { useCallCallingState } = useCallStateHooks();
   const callingState = useCallCallingState();
@@ -55,7 +56,7 @@ const MeetingRoom = () => {
       </div>
 
       <div className="fixed bottom-0 flex w-full items-center justify-center gap-5 flex-wrap">
-        <CallControls />
+        <CallControls onLeave={() => router.push('/')}/>
 
         <DropdownMenu>
 
--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -11,6 +11,7 @@ import { useToast } from "@/components/ui/use-toast"
 import ReactDatePicker from "react-datepicker"
 
 import { Textarea } from "./ui/textarea"
+import { Input } from "./ui/input"
 
 const MeetingTypeList = () => {
     const router = useRouter()
@@ -75,7 +76,7 @@ const MeetingTypeList = () => {
         }
     }
 
-    const meetingLink = `$(process.env.NEXT_PUBLIC_BASE_URL}/meeting/${callDetails?.id}`
+    const meetingLink = `${process.env.NEXT_PUBLIC_BASE_URL}/meeting/${callDetails?.id}`
 
     return (
         <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
@@ -161,6 +162,21 @@ const MeetingTypeList = () => {
                 buttonText="Start Meeting"
                 handleClick={createMeeting}
             />
+
+            <MeetingModal 
+                isOpen={meetingState === 'isJoiningMeeting'}
+                onClose={() => setMeetingState(undefined)}
+                title="Type the link here"
+                className="text-center"
+                buttonText="Join Meeting"
+                handleClick={() => router.push(values.link)}
+            >
+                <Input 
+                    placeholder="Meeting link"
+                    className="border-none bg-dark-3 focus-visible:ring-0 focus-visible:ring-offset-0"
+                    onChange={(e) => setValues({ ...values, link: e.target.value })}
+                />
+            </MeetingModal>
         </section>
     )
 }
